812677410df759d603bb010f0fb759eeb95598b1
Thom Stricklin <thomshouse@gmail.com>
2017-02-06 20:27:15 -0700

    Improve tmux-powerline themes and PuTTY support

--- tmux/powerline-theme.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-cp "`dirname $0`/powerline-theme" "$HOME/.tmux-powerline/themes/thomshouse-dotfiles.sh"
---
+++ tmux/powerline-themes.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+cp "`dirname $0`/powerline-theme" "$HOME/.tmux-powerline/themes/thomshouse-dotfiles.sh"
+cp "`dirname $0`/powerline-lofi" "$HOME/.tmux-powerline/themes/thomshouse-lofi.sh"
