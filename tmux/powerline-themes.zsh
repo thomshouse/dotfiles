299510aba45a30bc1d9baecc63249a417dc7c9da
Thom Stricklin <thomshouse@gmail.com>
2017-02-24 19:39:32 -0700

    Remove config related to tmux lofi theme

--- tmux/powerline-themes.zsh
+++ tmux/powerline-themes.zsh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 
 cp "`dirname $0`/powerline-theme" "$HOME/.tmux-powerline/themes/thomshouse-dotfiles.sh"
-cp "`dirname $0`/powerline-lofi" "$HOME/.tmux-powerline/themes/thomshouse-lofi.sh"
