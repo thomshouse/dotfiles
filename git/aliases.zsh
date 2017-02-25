6e8ae437abfdd18587a5510cd799bc5c19cdc5d8
Thom Stricklin <thomshouse@gmail.com>
2017-02-24 19:43:01 -0700

    Move npm gitignore alias to git/aliases.zsh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,9 @@
 git() {
-	if [ "$1" = top ]; then
+  if [ "$1" = top ]; then
     builtin cd `git rev-parse --show-toplevel`
   else
     command git "$@"
   fi
 }
+
+alias npm-generate-gitignore="cd node_modules 2> /dev/null && cd .. && echo '/*' > node_modules/.gitignore && echo '!/.gitignore' >> node_modules/.gitignore && npm ls --prod --parseable 2> /dev/null | grep node_modules | grep -vE 'node_modules.*node_modules' | sed 's/.*node_modules/!/' >> node_modules/.gitignore"
