04cbebe8eaa8ec1dfc612f7686cd275f60965c36
Thom Stricklin <thomshouse@gmail.com>
2018-03-01 20:24:19 -0700

    Fix pushkey function shell compatibility issue

--- system/keys.zsh
+++ system/keys.zsh
@@ -6,12 +6,12 @@ function pushkey() {
 		echo "Please include a server host domain or IP address to push your public key to.\n";
 		echo "Examples:\n  pushkey example.com\n  pushkey username@example.com\n";
 	else
-		ssh -o PreferredAuthentications=publickey -i ~/.ssh/id_rsa "$1" true >/dev/null 2>&1
-		if [ $? == 0 ]; then
-			echo "Public key is already added to this server."
+		ssh -o PreferredAuthentications=publickey -i ~/.ssh/id_rsa "$1" true >/dev/null 2>&1;
+		if [ "$?" -eq 0 ]; then
+			echo "Public key is already added to this server.";
 		else
-			cat ~/.ssh/id_rsa.pub | ssh "$1" 'cat >> .ssh/authorized_keys && chmod 600 .ssh/authorized_keys' 2>/dev/null
-			if [ $? == 0 ]; then
+			cat ~/.ssh/id_rsa.pub | ssh "$1" 'cat >> .ssh/authorized_keys && chmod 600 .ssh/authorized_keys' 2>/dev/null;
+			if [ "$?" -eq 0 ]; then
 				echo "Public key has been added to this server.";
 			else
 				echo "ERROR: Public key could not be added to server.";
