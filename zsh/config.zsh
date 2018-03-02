99de6e1ba204cad50a0867b408feec08c4307bfb
Thom Stricklin <thomshouse@gmail.com>
2018-03-01 20:36:39 -0700

    Configure zsh history to ignore if leading spaces

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -32,6 +32,7 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_SPACE # don't record commands beginning with a space
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
