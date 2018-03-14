56193b905d1e724fd1a2e3b9ded43c6e2fbfdf1f
Thom Stricklin <thomshouse@gmail.com>
2018-03-13 17:53:09 -0700

    Add conditional check to font install

--- fonts/install.sh
+++ fonts/install.sh
@@ -4,6 +4,8 @@
 #
 # Commands to run during installation 
 
-git clone https://github.com/powerline/fonts.git "$TMPDIR/powerline-fonts"
-find "$TMPDIR/powerline-fonts/SourceCodePro" -type f -name "*.otf" -exec cp {} ~/Library/Fonts \;
-rm -rf "$TMPDIR/powerline-fonts"
+if [ $(find ~/Library/Fonts -name "Source Code Pro*" | wc -l) -eq 0 ]; then
+  git clone https://github.com/powerline/fonts.git "$TMPDIR/powerline-fonts"
+  find "$TMPDIR/powerline-fonts/SourceCodePro" -type f -name "*.otf" -exec cp {} ~/Library/Fonts \;
+  rm -rf "$TMPDIR/powerline-fonts"
+fi
