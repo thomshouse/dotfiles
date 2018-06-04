338927e16e165aaeb2e134a2e2ff20a474c12416
Thom Stricklin <thomshouse@gmail.com>
2018-06-04 15:01:16 -0700

    Attempt to fix nvm lazy loading

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,21 +1,27 @@
 #!/bin/zsh
 
 nvm() {
-    unset -f nvm
+    unset -f nvm 2> /dev/null
+    unset -f node 2> /dev/null
+    unset -f npm 2> /dev/null
     export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     nvm "$@"
 }
 
 node() {
-    unset -f node
+    unset -f nvm 2> /dev/null
+    unset -f node 2> /dev/null
+    unset -f npm 2> /dev/null
     export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     node "$@"
 }
 
 npm() {
-    unset -f npm
+    unset -f nvm 2> /dev/null
+    unset -f node 2> /dev/null
+    unset -f npm 2> /dev/null
     export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     npm "$@"
