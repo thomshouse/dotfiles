08abaa7a9b6cf9559744c495a59be9ebbd605257
Thom Stricklin <thomshouse@gmail.com>
2018-06-11 09:28:10 -0700

    Fix nvm lazyload for all global packages

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,28 +1,19 @@
 #!/bin/zsh
 
-nvm() {
+lazyload_nvm() {
     unset -f nvm 2> /dev/null
-    unset -f node 2> /dev/null
-    unset -f npm 2> /dev/null
+    lazyload_nvm_get_commands | while read package ; do unset -f ${package} 2> /dev/null; done
     export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+}
+
+lazyload_nvm_get_commands() {
+    find .nvm/versions/node -type d -name "bin" -maxdepth 2 | xargs -I {} ls -1 {} | cat | sort | uniq
+}
+
+nvm() {
+    lazyload_nvm;
     nvm "$@"
 }
 
-node() {
-    unset -f nvm 2> /dev/null
-    unset -f node 2> /dev/null
-    unset -f npm 2> /dev/null
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    node "$@"
-}
-
-npm() {
-    unset -f nvm 2> /dev/null
-    unset -f node 2> /dev/null
-    unset -f npm 2> /dev/null
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    npm "$@"
-}
+lazyload_nvm_get_commands | while read package; do eval "${package}() { lazyload_nvm; ${package} \"\$@\" }"; done
