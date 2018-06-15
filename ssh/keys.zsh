abeb47b41ac44678409f0ae5d4772c846e268f4b
Thom Stricklin <thomshouse@gmail.com>
2018-06-15 16:27:00 -0700

    Add graceful interrupt support in ssh/keys.zsh

--- ssh/keys.zsh
+++ ssh/keys.zsh
@@ -2,6 +2,7 @@
 
 # Prompt to create id_rsa key-pair if it does not exist
 if [[ ! -e ~/.ssh/id_rsa ]]; then
+  trap "echo \"\"; break" SIGINT;
   while true; do
     read "yn?No SSH key (~/.ssh/id_rsa) found. Do you wish to create one? (Yn) "
     case $yn in
@@ -19,9 +20,12 @@ if [[ ! -e ~/.ssh/id_rsa ]]; then
       * ) echo "Please answer yes or no.";;
     esac
   done
+  trap - SIGINT;
 fi
 
 # Attempt to use homebrew "keychain" utility for ssh-agent
 if [[ $+commands[keychain] && -e ~/.ssh/id_rsa ]]; then
+  trap "" SIGINT;
   eval `keychain --quiet --nogui --eval --agents ssh id_rsa`;
+  trap - SIGINT;
 fi
