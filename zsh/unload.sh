f89bc63673aaafb32b2c77235abc34e2b113a0c7
Thom Stricklin <thomshouse@gmail.com>
2018-07-03 12:15:14 -0700

    Improve zgen unload adding update commands

--- zsh/unload.sh
+++ zsh/unload.sh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 
-if [ -z "$ZGEN_INIT" ]; then
-  ZGEN_INIT="$HOME/.zgen/init.zsh";
-fi
-if [[ -f "${ZGEN_INIT}" ]]
-then
-  echo "Deleting \"${ZGEN_INIT}\" ..."
-  rm "${ZGEN_INIT}"
-fi
+source "$HOME/.zgen/zgen.zsh"
+
+zgen update
+zgen selfupdate
