5ff134125f7ce13b0d8febf4bd64680572baf745
Thom Stricklin <thomshouse@gmail.com>
2019-10-21 14:26:24 -0700

    Fix PHP composer.zsh line formatting

--- php/composer.zsh
+++ php/composer.zsh
@@ -1,1 +1,5 @@
-export COMPOSER_HOME=$HOME/.config/composer                                                                                                                                                  export COMPOSER_CACHE_DIR=$COMPOSER_HOME/cache                                                                                                                                               if [ -f $COMPOSER_HOME/auth.json ]; then                                                                                                                                                       export COMPOSER_AUTH=$(cat $COMPOSER_HOME/auth.json)                                                                                                                                       fi
+export COMPOSER_HOME=$HOME/.config/composer
+export COMPOSER_CACHE_DIR=$COMPOSER_HOME/cache
+if [ -f $COMPOSER_HOME/auth.json ]; then
+    export COMPOSER_AUTH=$(cat $COMPOSER_HOME/auth.json)
+fi
